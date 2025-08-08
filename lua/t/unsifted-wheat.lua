b2067d91056b686082b766f3e82df2e5f6c7a951
Tox <toxtek@proton.me>
2025-08-08 03:21:56 -0500

    add unsifted wheat theme

---
+++ lua/t/unsifted-wheat.lua
@@ -0,0 +1,41 @@
+-- Base16 - Unsifted Wheat
+
+-- https://github.com/chriskempson/base16/blob/main/styling.md
+--
+--   base00 - Default Background
+--   base01 - Lighter Background (status bars, line numbers, folding marks)
+--   base02 - Selection Background
+--   base03 - Comments, Invisibles, Line Highlighting
+--   base04 - Dark Foreground (status bars)
+--   base05 - Default Foreground, Caret, Delimiters, Operators
+--   base06 - Light Foreground (rare)
+--   base07 - Light Background (rare)
+--
+--   base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+--   base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+--   base0A - Classes, Markup Bold, Search Text Background
+--   base0B - Strings, Inherited Class, Markup Code, Diff Inserted
+--   base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
+--   base0D - Functions, Methods, Attribute IDs, Headings
+--   base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
+--   base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+
+return {
+    base00 = "#111111",
+    base01 = "#30343e",
+    base02 = "#2d303a",
+    base03 = "#333333",
+    base04 = "#181a1e",
+    base05 = "#f6e5dd",
+    base06 = "#fff5fd",
+    base07 = "#30343e",
+
+    base08 = "#48393a",
+    base09 = "#65534d",
+    base0A = "#79665a",
+    base0B = "#836e5e",
+    base0C = "#8e7766",
+    base0D = "#917781",
+    base0E = "#382c2f",
+    base0F = "#d3d3d3",
+}
